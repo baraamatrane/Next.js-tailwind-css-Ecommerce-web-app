831417a08197aab570ba11d3d722fb02ccff28e2
baraa matrane <baraamatrane@gmail.com>
2024-04-20 16:44:29 +0100

    Update files

--- src/middleware.js
+++ src/middleware.js
@@ -3,24 +3,30 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const referer = request.headers.get("Referer");
 
+  // Extract cookie from the request headers
+  const token = request.headers.get("token");
+
   // Check if the user is authenticated but trying to access "/Signin" or "/Register"
   if (
     (request.nextUrl.pathname === "/Signin" ||
       request.nextUrl.pathname === "/Register") &&
-    request.cookies.get("token")
+    token
   ) {
-    return NextResponse.redirect("http://localhost:3000");
+    return NextResponse.redirect(
+      "https://next-js-tailwind-css-ecommerce-web-app-seven.vercel.app"
+    );
   }
-  console.log(request.cookies.get("mt"));
   if (
     (request.nextUrl.pathname === "/User/Account" ||
       request.nextUrl.pathname === "/User/Orders" ||
       request.nextUrl.pathname === "/User/Wishlist" ||
       request.nextUrl.pathname === "/User/ChangePassword" ||
       request.nextUrl.pathname === "/Checkout") &&
-    !request.cookies.get("token")
+    !token
   ) {
-    return NextResponse.redirect("http://localhost:3000/Signin");
+    return NextResponse.redirect(
+      "https://next-js-tailwind-css-ecommerce-web-app-seven.vercel.app//Signin"
+    );
   }
 
   // If none of the above conditions are met, allow the request to proceed
