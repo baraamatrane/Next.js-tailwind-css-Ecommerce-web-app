f0e7b6fbc40dbe76fe5019200195f79c389be6e7
baraa matrane <baraamatrane@gmail.com>
2024-04-21 15:15:49 +0100

    Update files

--- src/middleware.js
+++ src/middleware.js
@@ -3,19 +3,24 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const referer = request.headers.get("Referer");
 
-  // Extract cookie from the request headers
+  // Extract token from the request headers or cookies
   const token =
-    request.headers?.get("token") ||
-    request?.cookies?.get("token") ||
-    request?.headers?.cookies?.get("token");
+    request.headers
+      .get("cookie")
+      ?.split(";")
+      .find((c) => c.trim().startsWith("token="))
+      ?.split("=")[1] || request.cookies?.token;
+
   // Check if the user is authenticated but trying to access "/Signin" or "/Register"
   if (
     (request.nextUrl.pathname === "/Signin" ||
       request.nextUrl.pathname === "/Register") &&
     token
   ) {
-    return NextResponse.redirect(request.headers?.get("host"));
+    return NextResponse.redirect(request.headers.get("host"));
   }
+
+  // Check if the user is not authenticated but trying to access certain routes
   if (
     (request.nextUrl.pathname === "/User/Account" ||
       request.nextUrl.pathname === "/User/Orders" ||
@@ -24,7 +29,7 @@ export function middleware(request) {
       request.nextUrl.pathname === "/Checkout") &&
     !token
   ) {
-    return NextResponse.redirect(`${request.headers?.get("host")}/Signin`);
+    return NextResponse.redirect(`${request.headers.get("host")}/Signin`);
   }
 
   // If none of the above conditions are met, allow the request to proceed
